5832c8aff271062be24b95ae037da83a58ff356c
mahmpudfat7i <devmahmoudfathy@gmail.com>
2023-07-01 19:06:38 +0300

    e

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { useState } from 'react';
+import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UserList';
+
 function App() {
+  const [UsersList, setUsersList] = useState([]);
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUserList) => {
+      return [...prevUserList, {name: uName, age: uAge, id: Math.random().toString()}];
+    });
+  };
+
   return (
     <div>
-      
+      <AddUser onAddUser={addUserHandler} />
+      <UserList users={UsersList} />
     </div>
   );
 }
